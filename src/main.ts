6baa930656c862e063d5bac79768a040ed45a8c5
christian-bayata <franksagie1@gmail.com>
2024-01-29 18:22:58 +0100

    Switch ports

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const port = 3000;
+  await app.listen(port, () => logger.log(`App running on Port: ${port}`));
 }
 bootstrap();
